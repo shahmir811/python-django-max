65ae0109f0fc944321778568f2db0973b0a8d9d8
Shahmir Khan Jadoon <shahmir.khan@vartana.com>
2024-12-29 09:08:26 +0500

    Added template inheritance, blocks, includes, etc

--- monthly_challenges_01/challenges/views.py
+++ monthly_challenges_01/challenges/views.py
@@ -1,5 +1,4 @@
-from django.http import (HttpResponse, HttpResponseNotFound,
-                         HttpResponseRedirect)
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
@@ -48,4 +47,4 @@ def monthly_challenge(request, month):
         })
 
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
