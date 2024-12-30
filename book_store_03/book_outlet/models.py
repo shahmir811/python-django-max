8cfbf406000256268dd2a389dfe3bae20f494745
Shahmir Khan Jadoon <shahmir.khan@vartana.com>
2024-12-30 22:50:37 +0500

    Added slug in the creation of the record

--- book_store_03/book_outlet/models.py
+++ book_store_03/book_outlet/models.py
@@ -1,5 +1,7 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,6 +10,16 @@ class Book(models.Model): # table name will be books in the database
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]) # rating should be between 1 and 5  
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False) # default value is False
+    slug = models.SlugField(default="", null=False, unique=True)
+
+    def get_absolute_url(self):
+        return reverse("book_detail", kwargs={"book_id": self.id})
+    
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Book, self).save(*args, **kwargs)
+    
 
     def __str__(self):
         # Following code is used to display the title and rating of the book in the console
--- book_store_03/book_outlet/views.py
+++ book_store_03/book_outlet/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
+from .models import Book
 
 # Create your views here.
+def index(request):
+  books = Book.objects.all()
+  return render(request, 'book_outlet/index.html', {
+    "books": books
+  })
+
+
+def book_detail(request, book_id):
+
+  # try:
+  #   book = Book.objects.get(pk=book_id)
+  
+  # except:
+  #   raise Http404()
+
+  book = get_object_or_404(Book, pk=book_id)
+  return render(request, 'book_outlet/book_detail.html', {
+    "author": book.author,
+    "is_bestselling": book.is_bestselling,
+    "rating": book.rating,
+    "title": book.title,
+  })
+  
