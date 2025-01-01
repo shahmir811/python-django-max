f7545620d0f07346ce1269c1d4d416aa7964a0fb
Shahmir Khan Jadoon <shahmir.khan@vartana.com>
2025-01-01 12:53:57 +0500

    Added admin panel and relationships between different data

--- book_store_03/book_outlet/admin.py
+++ book_store_03/book_outlet/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
 
+from .models import Book, Author, Address, Country
+
+class AddressAdmin(admin.ModelAdmin):
+  list_display = ('street', 'postal_code', 'city')
+  search_fields = ('street', 'postal_code', 'city')
+
+class AuthorAdmin(admin.ModelAdmin):
+  list_display = ('first_name', 'last_name', 'address')
+  search_fields = ('first_name', 'last_name', 'address__street', 'address__postal_code', 'address__city')
+
+class BookAdmin(admin.ModelAdmin):
+  # readonly_fields = ('slug',)
+  prepopulated_fields = {'slug': ('title',)}
+  list_display = ('title', 'author', 'rating', 'is_bestselling', 'countries_display')
+  list_filter = ('author', 'rating')
+  search_fields = ('title', 'author__first_name', 'author__last_name')
+
+  def countries_display(self, obj):
+      return ", ".join(country.code for country in obj.published_countries.all())
+  countries_display.short_description = "Countries"  
+
+class CountryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'code', 'books_display',)
+  search_fields = ('name', 'code',)
+
+  def books_display(self, obj):
+      return ", ".join(book.title for book in obj.books.all())
+  books_display.short_description = "Books"
+
 # Register your models here.
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Address, AddressAdmin)
+admin.site.register(Country, CountryAdmin)
--- book_store_03/book_outlet/models.py
+++ book_store_03/book_outlet/models.py
@@ -5,22 +5,58 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Address(models.Model):
+    street = models.CharField(max_length=80)
+    postal_code = models.CharField(max_length=9)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.street}, {self.postal_code}, {self.city}"
+    
+    class Meta:
+        verbose_name_plural = "Address Entries"
+
+class Author(models.Model): # table name will be books in the database
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, related_name="author")
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self):
+        # Following code is used to display the title and rating of the book in the console and admin panel
+        return self.full_name()
+
 class Book(models.Model): # table name will be books in the database
     title = models.CharField(max_length=100)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]) # rating should be between 1 and 5  
-    author = models.CharField(null=True, max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books") # if author is deleted, all books by that author will also be deleted. 
     is_bestselling = models.BooleanField(default=False) # default value is False
-    slug = models.SlugField(default="", null=False, unique=True, db_index=True)
+    slug = models.SlugField(default="", blank=True,null=False, unique=True, db_index=True)
+    published_countries = models.ManyToManyField("Country", related_name="books")
 
     def get_absolute_url(self):
         return reverse("book_detail", kwargs={"slug": self.slug})
     
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super(Book, self).save(*args, **kwargs)
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super(Book, self).save(*args, **kwargs)
     
 
     def __str__(self):
-        # Following code is used to display the title and rating of the book in the console
+        # Following code is used to display the title and rating of the book in the console and admin panel
         return f"Title: {self.title}, Rating: {self.rating}, Author: {self.author}, Bestselling: {self.is_bestselling}"
+    
+
+class Country(models.Model):
+    name = models.CharField(max_length=80)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f"Name: {self.name}, Code: {self.code}"
+    
+    
+    class Meta:
+        verbose_name_plural = "Countries"    
